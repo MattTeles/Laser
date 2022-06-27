c1211eb8d8ecd9bf20df0efe55b90334d4a06529
MattTeles <80656787+MattTeles@users.noreply.github.com>
2022-06-26 22:11:50 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change")
+})
+
+document.querySelector(".close1").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change")
+})
+
+document.querySelector(".close2").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change")
+})
+
+document.querySelector(".close3").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change")
+})
+
+document.querySelector(".close4").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change")
+})
+
+
